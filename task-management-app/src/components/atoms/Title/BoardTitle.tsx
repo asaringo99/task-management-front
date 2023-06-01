a1f97c7ff63d5628fe3a057b44815e9f69596268
asaringo99 <ka99code@gmail.com>
2023-06-01 17:42:59 +0900

    title component

---
+++ task-management-app/src/components/atoms/Title/BoardTitle.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import styles from "./BoardTitle.module.css"
+
+type BoardTitlePropsType = {
+    title: string
+}
+
+const BoardTitle: FC<BoardTitlePropsType> = ({ title }) => {
+    return (
+        <div className={styles.boardTitle}>{title}</div>
+    );
+};
+
+export default BoardTitle
