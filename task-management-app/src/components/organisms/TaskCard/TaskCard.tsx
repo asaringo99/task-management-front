4996987b20f2d42aeaf87d8d006b4edcccb497a1
asaringo99 <ka99code@gmail.com>
2023-06-01 17:45:49 +0900

    [fix] taskcard

--- task-management-app/src/components/organisms/TaskCard/TaskCard.tsx
+++ task-management-app/src/components/organisms/TaskCard/TaskCard.tsx
@@ -1,24 +1,32 @@
 "use client"
 
 import React, { useState, FC } from 'react';
+import { useDrag } from 'react-dnd'
 import EditableText from '../../molecules/EditableText/EditableText';
 import TaskMenuwithButton from '../../molecules/Menu/TaskMenuWithButton'
 import CardFrame from '../../atoms/CardFrame/CardFrame'
+import { CARD_TYPE } from '../../types/dndtypes'
+import { CardType } from '../../types/card'
 
 interface TaskCardProps {
-    id: string
-    initialText: string;
-    menuItems: { label: string, action: () => void}[];
+    card: CardType
+    onEdit: (id: string, content: string) => void
 }
 
-const TaskCard: FC<TaskCardProps> = ({ id, initialText, menuItems }) => {
+const TaskCard: FC<TaskCardProps> = ({ card, onEdit }) => {
+    const[, drag] = useDrag(() => ({
+        type: CARD_TYPE,
+        item: {id: card.id}
+    }))
+
     const [isEditing, setIsEditing] = useState(false)
 
+
     return (
-        <div onClick={() => setIsEditing(true)} onBlur={() => setIsEditing(false)}>
+        <div ref={drag} onClick={() => setIsEditing(true)} onBlur={() => setIsEditing(false)}>
             <CardFrame>
-                    <TaskMenuwithButton menuItems={menuItems}/>
-                    <EditableText initialText={initialText} isEditing={isEditing}/>
+                    <TaskMenuwithButton menuItems={card.menuItems}/>
+                    <EditableText initialText={card.text} isEditing={isEditing} onEdit={(content: string) => onEdit(card.id, content)}/>
             </CardFrame>
         </div>
     );
