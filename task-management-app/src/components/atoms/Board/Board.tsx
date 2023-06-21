589cfe4798f64145194db5193c6057f2502ea778
asaringo99 <ka99code@gmail.com>
2023-06-22 00:49:11 +0900

    [fix] board

--- task-management-app/src/components/atoms/Board/Board.tsx
+++ task-management-app/src/components/atoms/Board/Board.tsx
@@ -2,20 +2,13 @@ import React, { FC } from 'react';
 import styles from './Board.module.css'
 
 interface BoardProps {
-    title: string
     children?: React.ReactNode;
 }
 
-const Board: FC<BoardProps> = ({ title, children }) => {
-
-    return (
-        <>
-            <h2 className={styles.title}>{title}</h2>
-            <div className={styles.board}>
-                <div>{children}</div>
-            </div>
-        </>
-    );
-};
+const Board: FC<BoardProps> = ({ children }) => (
+    <div className={styles.board}>
+        <div>{children}</div>
+    </div>
+);
 
 export default Board;
