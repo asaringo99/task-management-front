3eea9b4856c3c998b1ea97ea274bb336a4c8f31e
asaringo99 <ka99code@gmail.com>
2023-06-22 00:50:42 +0900

    [update] template

--- task-management-app/src/components/templates/KanbanBoard/KanbanBoard.tsx
+++ task-management-app/src/components/templates/KanbanBoard/KanbanBoard.tsx
@@ -1,91 +1,124 @@
 'use client'
 
-import React, { useState, FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import BoardWithButton from '../../molecules/BoardWithButton/BoardWithButton';
-import TaskCard from '../../organisms/TaskCard/TaskCard';
-import AddButton from '../../atoms/Button/AddButton';
 import { CardType } from '../../types/card'
 import { BoardType } from '../../types/board'
-import { postData } from '../../../services/api'
 import styles from './KanbanBoard.module.css'
+import ApiClient from '@/services/api/client';
+import { ApiResponse, DataTaskCard, DataTaskBoard, DataBoardid, DataTaskTab } from '@/services/api/response';
+import AddBoardButton from '../../organisms/AddBoardButton/AddBoardButton';
+import TaskBoard from '../../organisms/TaskBoard/TaskBoard';
+import ScrollableContainer from '../../atoms/Container/ScrollableContainer';
+import TaskCard from '../../organisms/TaskCard/TaskCard';
+import TabContainer from '../../organisms/TabContainer/TabContainer';
+import { TabType } from '../../types/tab';
 
 interface KanbanBoardProps {
-    initialBoards: BoardType[];
-    initialCards: CardType[];
+    initialTitle: string
+    initialText: string
+    initialMenus: {label: string, action: () => void}[]
 }
 
-const KanbanBoard: FC<KanbanBoardProps> = ({ initialBoards, initialCards }) => {
-    const[boards, setBoards] = useState<BoardType[]>(initialBoards);
-    const[cards, setCards] = useState<CardType[]>(initialCards);
+const KanbanBoard: FC<KanbanBoardProps> = ({ initialTitle, initialText, initialMenus }) => {
+    const[activeTabid, setActiveTabid] = useState<number>(-1)
+    const [tabs, setTabs] = useState<TabType[]>([]);
+    const[boards, setBoards] = useState<BoardType[]>([]);
+    const[cards, setCards] = useState<CardType[]>([]);
 
-    const editCard = (id: string, content: string) => {
-        setCards((prevCards) => prevCards.map((card) => (card.id === id ? { ...card, text: content } : card)));
-    }
+    const clientEntryPoint = "http://localhost:8080"
+    const apiClient = new ApiClient(clientEntryPoint);
     
-    const initialTitle = 'New Board'
-    const initialText = 'Input Contents'
-    const initialMenus = [
-        { label: "color", action: () => {console.log("color")} },
-        { label: "delete", action: () => {console.log("delete")} },
-    ]
+    useEffect(() => {
+        const tabPath = '/tab/get'
+        apiClient.get<ApiResponse<DataTaskTab[]>>(tabPath).then(response => {
+            const newTabList: TabType[] = response.data.data.map((taskTab) => {
+                const tab: TabType = {
+                    tabid: taskTab.tabid,
+                    isactive: taskTab.isactive,
+                    title: taskTab.title,
+                }
+                return tab
+            });
+            setTabs(newTabList)
+            const activeTab = newTabList.filter((tab) => tab.isactive)
+            setActiveTabid(activeTab.length > 0 ? activeTab[0].tabid : newTabList[0].tabid)
+        }).catch(error => {console.log(error)})
+    }, [])
 
-    const addBoard = async () => {
-        const payload = {'title': initialTitle};
-        const response = await postData(payload);
-        const id: string = response['id'];
-        const newBoard: BoardType = { id: id, title: initialTitle, cardIds: []};
-        const newBoardList: BoardType[] = [...boards, newBoard];
-        setBoards(newBoardList);
-    }
+    useEffect(() => {
+        const boardPath = '/board/get'
+        apiClient.get<ApiResponse<DataTaskBoard[]>>(boardPath).then(response => {
+            const newBoardList: BoardType[] = response.data.data.map((taskBoard) => {
+                const board: BoardType = {
+                    id: taskBoard.boardid,
+                    tabid: taskBoard.tabid,
+                    status: taskBoard.status,
+                }
+                return board
+            });
+            console.log(newBoardList)
+            setBoards(newBoardList)
+        }).catch(error => {console.log(error)})
+    }, [])
+
+    useEffect(() => {
+        const cardPath = '/task/get'
+        apiClient.get<ApiResponse<DataTaskCard[]>>(cardPath).then(response => {
+            console.log(response.data)
+            const newCardList: CardType[] = response.data.data.map((taskCard) => {
+                const task: CardType = {
+                    id: taskCard.taskid,
+                    boardId: taskCard.boardid,
+                    text: taskCard.contents,
+                    menuItems: initialMenus,
+                }
+                return task
+            })
+            setCards(newCardList)
+        }).catch(error => {console.log(error)})
+    }, [])
     
-    const addCard = async (boardId: string) => {
-        const payload = {'title': initialText};
-        const response = await postData(payload);
-        const id: string = response['id'];
-        const newCard: CardType = { id: id, text: initialText, menuItems: initialMenus};
-        const newCardList: CardType[] = [newCard, ...cards];
-        setCards(newCardList);
-        setBoards((prevBoards) => 
-            prevBoards.map((board) => (board.id === boardId ? { ...board, cards: [...board.cardIds, newCard.id] } : board))
-        );
-    }
-
-    const onDrop = (cardId: string, toBoardId: string) => {
-        setBoards((prevBoards) =>
-            prevBoards.map((board) => ({
-                ...board,
-                cardIds: board.cardIds.filter((id) => id !== cardId).concat(board.id === toBoardId ? cardId: [])
-            }))
-        )
+    const onBoardDelete = (boardid: string) => {
+        const boardPath = `/board/delete/${boardid}`
+        apiClient.delete<ApiResponse<any>>(boardPath).then(response => {
+            console.log(response)
+        }).catch(error => {
+            console.log(error)
+        })
+        const newBoardList = boards.filter((board) => board.id !== boardid)
+        setBoards(newBoardList)
     }
 
     return(
         <>
-            <div className={styles.kanbanBoard}>
-                <DndProvider backend={HTML5Backend}>
-                    {boards.map((board) => (
-                        <div>
-                            <BoardWithButton
-                                key={board.id}
-                                borad={board}
-                                addAction={addCard}
-                                onDrop={onDrop}
-                            >
-                                {
-                                    (cards.filter((card) => board.cardIds.includes(card.id))).map((card) => (
-                                        <TaskCard key={card.id} card={card} onEdit={editCard}/>
-                                    ))
-                                }
-                            </BoardWithButton>
-                        </div>
-                    ))}
-                </DndProvider>
-                <div className={styles.addBoardButton}>
-                    <AddButton label='+' onClick={addBoard}/>
+            <h1 className={styles.title}>KANBAN BOARD</h1>
+            <TabContainer apiClient={apiClient} activeTabid={activeTabid} setActiveTabid={setActiveTabid} tabs={tabs} setTabs={setTabs}>
+                <div className={styles.kanbanBoard}>
+                    <DndProvider backend={HTML5Backend}>
+                        {(boards.filter((b) => b.tabid === activeTabid)).map((board) => (
+                            <div>
+                                <TaskBoard apiClient={apiClient} setBoards={setBoards} initialText={initialText} initialMenus={initialMenus} board={board} cards={cards} setCards={setCards} onDelete={onBoardDelete}>
+                                    <ScrollableContainer>
+                                        {
+                                            (cards.filter((card) => board.id === card.boardId)).map((card) => (
+                                                <TaskCard key={card.id} card={card} setCards={setCards} apiClient={apiClient}/>
+                                            ))
+                                        }
+                                    </ScrollableContainer>
+                                </TaskBoard>
+                            </div>
+                        ))}
+                    </DndProvider>
+                    {
+                        tabs.length > 0 &&
+                            <div className={styles.addBoardButton}>
+                                <AddBoardButton initialTitle={initialTitle} tabid={activeTabid} boards={boards} setBoards={setBoards} apiClient={apiClient}/>
+                            </div>
+                    }
                 </div>
-            </div>
+            </TabContainer>
         </>
     )
 }
