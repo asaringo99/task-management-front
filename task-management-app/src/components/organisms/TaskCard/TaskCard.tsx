a37492c418dfa4122cd93c2a17cc492a2daf38f1
asaringo99 <ka99code@gmail.com>
2023-06-22 00:49:00 +0900

    [fix] taskcard

--- task-management-app/src/components/organisms/TaskCard/TaskCard.tsx
+++ task-management-app/src/components/organisms/TaskCard/TaskCard.tsx
@@ -8,26 +8,41 @@ import CardFrame from '../../atoms/CardFrame/CardFrame'
 import { CARD_TYPE } from '../../types/dndtypes'
 import { CardType } from '../../types/card'
 import styles from './TaskCard.module.css'
+import ApiClient from '@/services/api/client';
 
 interface TaskCardProps {
     card: CardType
-    onEdit: (id: string, content: string) => void
+    apiClient: ApiClient
+    setCards: (value: React.SetStateAction<CardType[]>) => void
 }
 
-const TaskCard: FC<TaskCardProps> = ({ card, onEdit }) => {
+const TaskCard: FC<TaskCardProps> = ({ card, setCards, apiClient }) => {
+    const [isEditing, setIsEditing] = useState(false)
+
+    const onEdit = (card: CardType, content: string) => {
+        setCards((prevCards) => prevCards.map((prevCard) => (prevCard.id === card.id ? { ...prevCard, text: content } : prevCard)));
+        const joinedPath = `/task/put/${card.id}`
+        const payload = {
+            'boardid': card.boardId, 
+            'contents': content,
+        }
+        apiClient.put(joinedPath, payload).then(response => {
+            console.log(response)
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
     const[, drag] = useDrag(() => ({
         type: CARD_TYPE,
         item: {id: card.id}
     }))
 
-    const [isEditing, setIsEditing] = useState(false)
-
-
     return (
         <div className={styles.taskCard} ref={drag} onClick={() => setIsEditing(true)} onBlur={() => setIsEditing(false)}>
             <CardFrame>
                 <TaskMenuwithButton menuItems={card.menuItems}/>
-                <EditableText initialText={card.text} isEditing={isEditing} onEdit={(content: string) => onEdit(card.id, content)}/>
+                <EditableText initialText={card.text} isEditing={isEditing} onEdit={(content: string) => onEdit(card, content)}/>
             </CardFrame>
         </div>
     );
